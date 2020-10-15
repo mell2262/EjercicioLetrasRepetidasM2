d0a03368397b52ab79c788d20f2ac79fe2c64bb7
mell2262 <mellgomez.2262@gmail.com>
2020-10-15 16:32:29 +0200

    Fase 3

--- src/LetrasRepetidas.java
+++ src/LetrasRepetidas.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class LetrasRepetidas {
@@ -6,31 +7,31 @@ public class LetrasRepetidas {
 	public static void main(String[] args) {
 		/* El ejercicio consiste en mostrar por consola cuántas veces aparece cada letra 
 		 * de tu nombre y apellidos con diferentes variantes (fases).
-		FASE 2
-		 Cambia la tabla  para una lista (List <Character>)
-		 En bucle, si la letra es una vocal imprime en la consola: 'VOCAL'. Sino, imprime: 
-		'CONSONTANT'.
-		 Si encuentras un numero, muestra por pantalla: 'Los nombres de personas no .
-		contienen números!'.
+		FASE 3
+		 Almacenar en un Map tanto las letras de la lista como el número de veces 
+		que aparecen.
 
 		 */
 		List<Character> nombre = new ArrayList<Character>();
-		
+		HashMap<Character, Integer> repetLetras= new HashMap<Character, Integer>();
+			
 		nombre.add('M');
 		nombre.add('E');
-		nombre.add('l');
-		nombre.add('i');
+		nombre.add('L');
+		nombre.add('I');
 		nombre.add('S');
-		nombre.add('8');
+		nombre.add('E');
+		
 		
 		for (int i=0; i<nombre.size();i++) {
-			if ((nombre.get(i).equals('a')) || (nombre.get(i).equals('e')) ||(nombre.get(i).equals('i')) ||(nombre.get(i).equals('o')) ||(nombre.get(i).equals('u')) ||(nombre.get(i).equals('A')) ||(nombre.get(i).equals('E')) ||(nombre.get(i).equals('I')) ||(nombre.get(i).equals('O')) ||(nombre.get(i).equals('U'))) 
-				System.out.println(nombre.get(i)+ " VOCAL");
-			else 
-				if ((nombre.get(i).charValue() >= 48) && (nombre.get(i).charValue()<= 57))
-					System.out.println("Los nombres de personas no contienen números!");
-				else System.out.println(nombre.get(i)+ " CONSONANATE");
-
+			
+			if (repetLetras.get(nombre.get(i))==null) repetLetras.put(nombre.get(i), 1);
+			else {
+				Integer a=repetLetras.get(nombre.get(i));
+				repetLetras.replace(nombre.get(i), a+1);
+			}
 		}
+		System.out.println(repetLetras);
+		
 	}
 }
